697a340ae23b35560902de53f9a1601bfe730221
David Négrier <david@davidtcm>
2012-10-19 16:06:01 +0200

    Keeping development

--- install.php
+++ install.php
@@ -1,8 +1,9 @@
 <?php
-require_once __DIR__."/../../autoload.php";
-
+use Mouf\Html\Utils\WebLibraryManager\WebLibraryInstaller;
 use Mouf\Actions\InstallUtils;
 use Mouf\MoufManager;
+
+require_once __DIR__."/../../autoload.php";
 
 // Let's init Mouf
 InstallUtils::init(InstallUtils::$INIT_APP);
@@ -10,7 +11,7 @@ InstallUtils::init(InstallUtils::$INIT_APP);
 // Let's create the instance
 $moufManager = MoufManager::getMoufManager();
 
-WebLibraryInstaller::installLibrary("javascript.jquery-filetree",
+WebLibraryInstaller::installLibrary("javascript.bootstrap",
 	array(
 		'vendor/mouf/html.utils.bootstrap/js/bootstrap.min.js'
 	),
